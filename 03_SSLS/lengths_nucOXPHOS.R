84d6529822cf62873a12e349adc5bdf325763aa6
Oscar Wallnoefer <123078003+oscarwallnoefer@users.noreply.github.com>
2024-09-30 16:39:18 +0200

    Create lengths_nucOXPHOS.R

---
+++ 03_SSLS/lengths_nucOXPHOS.R
@@ -0,0 +1,89 @@
+lunghezze_CI <- c(
+  lunghezza_NDUFA1 <- length(vettori_genes$NDUFA1),
+  lunghezza_NDUFA2 <- length(vettori_genes$NDUFA2),
+  lunghezza_NDUFA3 <- length(vettori_genes$NDUFA3),
+  lunghezza_NDUFA4 <- length(vettori_genes$NDUFA4),
+  lunghezza_NDUFA5 <- length(vettori_genes$NDUFA5),
+  lunghezza_NDUFA6 <- length(vettori_genes$NDUFA6),
+  lunghezza_NDUFA7 <- length(vettori_genes$NDUFA7),
+  lunghezza_NDUFA8 <- length(vettori_genes$NDUFA8),
+  lunghezza_NDUFA9 <- length(vettori_genes$NDUFA9),
+  lunghezza_NDUFA10 <- length(vettori_genes$NDUFA10),
+  lunghezza_NDUFA11 <- length(vettori_genes$NDUFA11),
+  lunghezza_NDUFA12 <- length(vettori_genes$NDUFA12),
+  lunghezza_NDUFA13 <- length(vettori_genes$NDUFA13),
+  lunghezza_NDUFAB1 <- length(vettori_genes$NDUFAB1),
+  lunghezza_NDUFB1 <- length(vettori_genes$NDUFB1),
+  lunghezza_NDUFB2 <- length(vettori_genes$NDUFB2),
+  lunghezza_NDUFB3 <- length(vettori_genes$NDUFB3),
+  lunghezza_NDUFB4 <- length(vettori_genes$NDUFB4),
+  lunghezza_NDUFB5 <- length(vettori_genes$NDUFB5),
+  lunghezza_NDUFB6 <- length(vettori_genes$NDUFB6),
+  lunghezza_NDUFB7 <- length(vettori_genes$NDUFB7),
+  lunghezza_NDUFB8 <- length(vettori_genes$NDUFB8),
+  lunghezza_NDUFB9 <- length(vettori_genes$NDUFB9),
+  lunghezza_NDUFB10 <- length(vettori_genes$NDUFB10),
+  lunghezza_NDUFB11 <- length(vettori_genes$NDUFB11),
+  lunghezza_NDUFC1 <- length(vettori_genes$NDUFC1),
+  lunghezza_NDUFC2 <- length(vettori_genes$NDUFC2),
+  lunghezza_NDUFS1 <- length(vettori_genes$NDUFS1),
+  lunghezza_NDUFS2 <- length(vettori_genes$NDUFS2),
+  lunghezza_NDUFS3 <- length(vettori_genes$NDUFS3),
+  lunghezza_NDUFS4 <- length(vettori_genes$NDUFS4),
+  lunghezza_NDUFS5 <- length(vettori_genes$NDUFS5),
+  lunghezza_NDUFS6 <- length(vettori_genes$NDUFS6),
+  lunghezza_NDUFS7 <- length(vettori_genes$NDUFS7),
+  lunghezza_NDUFS8 <- length(vettori_genes$NDUFS8),
+  lunghezza_NDUFV1 <- length(vettori_genes$NDUFV1),
+  lunghezza_NDUFV2 <- length(vettori_genes$NDUFV2),
+  lunghezza_NDUFV3 <- length(vettori_genes$NDUFV3)
+)
+
+
+
+lunghezze_CII <- c(
+    lunghezza_SDHA,
+    lunghezza_SDHB,
+    lunghezza_SDH3,
+    lunghezza_SDH4
+)
+
+lunghezze_CIII <- c(
+  lunghezza_QCR2 <- length(vettori_genes$QCR2),
+  lunghezza_QCR6 <- length(vettori_genes$QCR6),
+  lunghezza_QCR7 <- length(vettori_genes$QCR7),
+  lunghezza_QCR8 <- length(vettori_genes$QCR8),
+  lunghezza_QCR9 <- length(vettori_genes$QCR9),
+  lunghezza_QCR10 <- length(vettori_genes$QCR10),
+  lunghezza_CYC1 <- length(vettori_genes$CYC1),
+  lunghezza_UQCRFS1 <- length(vettori_genes$UQCRFS1)
+)
+
+lunghezze_CIV <- c(
+  lunghezza_COX4 <- length(vettori_genes$COX4),
+  lunghezza_COX5A <- length(vettori_genes$COX5A),
+  lunghezza_COX5B <- length(vettori_genes$COX5B),
+  lunghezza_COX6A <- length(vettori_genes$COX6A),
+  lunghezza_COX6B <- length(vettori_genes$COX6B),
+  lunghezza_COX6C <- length(vettori_genes$COX6C),
+  lunghezza_COX7A <- length(vettori_genes$COX7A),
+  lunghezza_COX7B <- length(vettori_genes$COX7B),
+  lunghezza_COX7C <- length(vettori_genes$COX7C),
+  lunghezza_COX10 <- length(vettori_genes$COX10),
+  lunghezza_COX11 <- length(vettori_genes$COX11),
+  lunghezza_COX15 <- length(vettori_genes$COX15),
+  lunghezza_COX17 <- length(vettori_genes$COX17)
+)
+
+lunghezze_CV <- c(
+    lunghezza_ATP5A1 <- length(vettori_genes$ATP5A1),
+    lunghezza_ATP5B <- length(vettori_genes$ATP5B),
+    lunghezza_ATP5C1 <- length(vettori_genes$ATP5C1),
+    lunghezza_ATP5D <- length(vettori_genes$ATP5D),
+    lunghezza_ATP5E <- length(vettori_genes$ATP5E),
+    lunghezza_ATP5F1 <- length(vettori_genes$ATP5F1),
+    lunghezza_ATP5G <- length(vettori_genes$ATP5G),
+    lunghezza_ATP5J <- length(vettori_genes$ATP5J),
+    lunghezza_ATP5L <- length(vettori_genes$ATP5L),
+    lunghezza_ATP5O <- length(vettori_genes$ATP5O)
+)
