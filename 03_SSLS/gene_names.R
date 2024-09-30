3cfc685087728dbcc3ba321a17e082edaafadd89
Oscar Wallnoefer <123078003+oscarwallnoefer@users.noreply.github.com>
2024-09-30 16:51:33 +0200

    Create gene_names.R

---
+++ 03_SSLS/gene_names.R
@@ -0,0 +1,26 @@
+# Nomi geni per il complesso I
+nomi_geni_CI <- c("NDUFA1", "NDUFA2", "NDUFA3", "NDUFA4", "NDUFA5", 
+                  "NDUFA6", "NDUFA7", "NDUFA8", "NDUFA9", "NDUFA10", 
+                  "NDUFA11", "NDUFA12", "NDUFA13", "NDUFAB1", 
+                  "NDUFB1", "NDUFB2", "NDUFB3", "NDUFB4", 
+                  "NDUFB5", "NDUFB6", "NDUFB7", "NDUFB8", 
+                  "NDUFB9", "NDUFB10", "NDUFB11", "NDUFC1", 
+                  "NDUFC2", "NDUFS1", "NDUFS2", "NDUFS3", 
+                  "NDUFS4", "NDUFS5", "NDUFS6", "NDUFS7", 
+                  "NDUFS8", "NDUFV1", "NDUFV2", "NDUFV3")
+
+# Nomi geni per il complesso II
+nomi_geni_CII <- c("SDHA", "SDHB", "SDH3", "SDH4")
+
+# Nomi geni per il complesso III
+nomi_geni_CIII <- c("QCR2", "QCR6", "QCR7", "QCR8", "QCR9", 
+                    "QCR10", "CYC1", "UQCRFS1")
+
+# Nomi geni per il complesso IV
+nomi_geni_CIV <- c("COX4", "COX5A", "COX5B", "COX6A", "COX6B", 
+                   "COX6C", "COX7A", "COX7B", "COX7C", 
+                   "COX10", "COX11", "COX15", "COX17")
+
+# Nomi geni per il complesso V
+nomi_geni_CV <- c("ATP5A1", "ATP5B", "ATP5C1", "ATP5D", "ATP5E", 
+                  "ATP5F1", "ATP5G", "ATP5J", "ATP5L", "ATP5O")
