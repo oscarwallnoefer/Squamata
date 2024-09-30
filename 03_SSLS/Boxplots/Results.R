07e2bf3377cf9a84c3f56cc5ceaaa4d0081f8efe
Oscar Wallnoefer <123078003+oscarwallnoefer@users.noreply.github.com>
2024-09-30 19:53:26 +0200

    Create Results.R

---
+++ 03_SSLS/Boxplots/Results.R
@@ -0,0 +1,25 @@
+> print(mw_test)
+
+	Wilcoxon rank sum test with continuity correction
+
+data:  Valore by Tipo
+W = 191425, p-value = 0.001597
+alternative hypothesis: true location shift is not equal to 0
+
+> 
+> # Riepilogo delle statistiche per entrambi i gruppi
+> riepilogo_valori <- valori_geni %>%
++     group_by(Tipo) %>%
++     summarise(
++         Media = mean(Valore, na.rm = TRUE),
++         Mediana = median(Valore, na.rm = TRUE),
++         Deviazione_Standard = sd(Valore, na.rm = TRUE),
++         N = n()
++     )
+> 
+> print(riepilogo_valori)
+# A tibble: 2 × 5
+  Tipo          Media Mediana Deviazione_Standard     N
+  <chr>         <dbl>   <dbl>               <dbl> <int>
+1 Contatto     -0.401  -0.640                1.28   654
+2 Non Contatto -0.613  -0.776                1.23   529
