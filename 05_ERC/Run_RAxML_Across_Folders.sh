293c8649972312a076cee46bad588fe7da10cb11
Oscar Wallnoefer <123078003+oscarwallnoefer@users.noreply.github.com>
2024-10-03 10:44:51 +0200

    Update Run_RAxML_Across_Folders.sh

--- 05_ERC/Run_RAxML_Across_Folders.sh
+++ 05_ERC/Run_RAxML_Across_Folders.sh
@@ -1,1 +1,12 @@
-nrt
+#!/bin/bash
+# Script to run raxml across subfolders (Iteration_*/Block*)
+main_directory="Iterations"
+for iteration_folder in "$main_directory"/Iteration_*/; do
+    for block_folder in "$iteration_folder"/Block*/; do
+        cd "$block_folder" || exit
+
+        raxmlHPC -f e -t ortho_erc_test.tree -m GTRGAMMA -s concatenated.out -q partitions.txt -n TEST
+
+        cd - || exit
+    done
+done
