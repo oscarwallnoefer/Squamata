a437afdd7b587095c5e93988890d255557e429f9
Oscar Wallnoefer <123078003+oscarwallnoefer@users.noreply.github.com>
2024-10-03 10:07:27 +0200

    Create Curve_Stats.R

---
+++ 05_ERC/Curve_Stats.R
@@ -0,0 +1,21 @@
+### Summarize density curves
+
+# Control Curve Busco-nucOXPHOS
+> summary(table_rho_Block12$V3) 
+    Min.  1st Qu.   Median     Mean  3rd Qu.     Max. 
+-0.59532 -0.09489  0.10044  0.08637  0.26610  0.67329
+
+# Control Curve Busco-mtOXPHOS 
+> summary(table_rho_Block34$V3)
+   Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+-0.6662 -0.1158  0.1197  0.1140  0.3684  0.8033
+ 
+# Control Curve nucOXPHOS-mtOXPHOS 
+> summary(table_rho_nucOXPHOS_mt$V1)
+   Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+ 0.5047  0.6926  0.7236  0.7182  0.7499  0.8127 
+
+# Control Curve Contact_nucOXPHSO-mtOXPHOS 
+> summary(table_rho_contact_nucOXPHOS_mt$V1)
+   Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+ 0.6437  0.7722  0.7921  0.7882  0.8090  0.8568
