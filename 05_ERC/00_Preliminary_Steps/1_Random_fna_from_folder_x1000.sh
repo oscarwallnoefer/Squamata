8efc60ea72f3174f68faed2cc5cd1ef64609c9ab
Oscar Wallnoefer <123078003+oscarwallnoefer@users.noreply.github.com>
2024-11-07 09:47:00 +0100

    Update 1_Random_fna_from_folder_x1000.sh

--- 05_ERC/00_Preliminary_Steps/1_Random_fna_from_folder_x1000.sh
+++ 05_ERC/00_Preliminary_Steps/1_Random_fna_from_folder_x1000.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # Script to extract 1000 random files (88+88+13+13)
 for (( count=0; count<1000; count++ )); do
-    block1=$(ls *.fna | shuf -n 88)
-    block2=$(ls *.fna | grep -v "$(echo "$block1" | tr ' ' '|')" | shuf -n 88)
+    block1=$(ls *.fna | shuf -n 73)
+    block2=$(ls *.fna | grep -v "$(echo "$block1" | tr ' ' '|')" | shuf -n 73)
     block3=$(ls *.fna | grep -v "$(echo "$block1 $block2" | tr ' ' '|')" | shuf -n 13)
     block4=$(ls *.fna | grep -v "$(echo "$block1 $block2 $block3" | tr ' ' '|')" | shuf -n 13)
     
     combined_files=$(echo "$block1 $block2 $block3 $block4" | tr ' ' '\n' | sort | uniq)
 
-    # Check sum of files = 202
-    if [ $(echo "$combined_files" | wc -l) -eq 202 ]; then
+    # Check sum of files = 172
+    if [ $(echo "$combined_files" | wc -l) -eq 172 ]; then
         output_dir="Iteration_$count"
         mkdir -p "$output_dir"/Block{1,2,3,4}
         cp $block1 "$output_dir"/Block1/
